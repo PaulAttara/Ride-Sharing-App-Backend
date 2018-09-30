efa3bb249456f7ccd599a59b28ddb1140c054427
Noam Suissa <noam.suissa01@gmail.com>
2018-09-29 20:25:44 -0400

    updated app class

--- src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
@@ -1,12 +1,20 @@
 package ca.mcgill.ecse321.ridesharing;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RestController
 @SpringBootApplication
 public class RideSharingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RideSharingApplication.class, args);
 	}
+
+  @RequestMapping("/")
+  public String greeting(){
+    return "Hello world!";
+  }
 }
