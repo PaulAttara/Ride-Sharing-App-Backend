2f4ae5d982a2e5b9ba6fe943f691dc8dda1cdb0b
Noam Suissa <noam.suissa01@gmail.com>
2018-09-29 20:09:50 -0400

    initial commit of backend

---
+++ src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
@@ -0,0 +1,12 @@
+package ca.mcgill.ecse321.ridesharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideSharingApplication.class, args);
+	}
+}
