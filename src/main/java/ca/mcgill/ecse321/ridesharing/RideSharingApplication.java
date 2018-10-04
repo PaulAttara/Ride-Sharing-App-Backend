8a97a6767996b3b61e905680a6adae60b0fa996e
Noam Suissa <noam.suissa01@gmail.com>
2018-10-03 22:58:35 -0400

    testing travis

--- src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RideSharingApplication {
-
+	//testing to push
 	public static void main(String[] args) {
 		SpringApplication.run(RideSharingApplication.class, args);
 	}
