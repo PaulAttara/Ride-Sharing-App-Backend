eb360f4379eff8ab16519c0e697eb7f8874c6e7c
Noam Suissa <noam.suissa01@gmail.com>
2018-10-07 19:19:14 -0400

    modified dependencies for unit test

--- src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
@@ -1,11 +1,17 @@
 package ca.mcgill.ecse321.ridesharing;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class RideSharingApplicationTests{
-@Test
-public void contextLoads ()
-{
-}
+public class EventregistrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
 }
