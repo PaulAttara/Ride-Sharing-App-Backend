099b63d4965924e6d19819b05967dfdf6ed78b82
Lucas Bellido <lucasbellido98@hotmail.com>
2018-10-07 18:01:07 -0400

    controller methods put into the right class

--- src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
@@ -24,63 +24,6 @@ public class RideSharingController {
 	public String greeting() {
 		return "Hello world!";
 	}
-	// this method updates the seats available in a route depending on the status of the route
-	public void updateSeatsAvail (Route aRoute) {
-	int seatsAvail= aRoute.getSeatsAvailable();
-	Set <Request> requests = aRoute.getRequest();
-	for(Request r: requests) {
-		Status s = r.getStatus();
-		if (s==s.Accepted) {
-			seatsAvail--;
-		}
-		else if (s==s.Ended) {
-			seatsAvail++;
-		}
-		
-	}
-	aRoute.setSeatsAvailable(seatsAvail);
-}
-// this method should be called when a passenger is dropped off
-// this gives the driver the chance to rate the passenger
-// this also increments the num of past trips for the passenger and updates his rating
-// this also updates the status of the specific request of the passenger to ended
-public void passengerDroppedOff (Route aRoute, double rating, Passenger aPassenger) {
-	
-		Set <Request> requests = aRoute.getRequest();
-		for(Request r: requests) {
-			
-			Passenger p =r.getPassenger();
-			if (p ==aPassenger) {
-				r.setStatus(Status.Ended);
-				double currentRating=aPassenger.getAvgRating();
-				double numPastTrips=(double)aPassenger.getNumOfPastTrips();
-			
-				currentRating= ((rating+currentRating*(numPastTrips))/(numPastTrips+1));
-				aPassenger.setNumOfPastTrips((int)numPastTrips+1);
-				aPassenger.setAvgRating(currentRating);
-			}
-		
-		
-	}
-}
-//A passenger will call this when he's dropped off and status of request is ended
-//the route only has one driver so it is not necessary to take in a role
-// this updates the drivers rating and the number of trips
-// this should only be called when the passengerDroppedOff method has been called
-// the status MUST BE = ENDED
-public void passengerRatesDriver (double rating, Route aRoute) {
-	
-		Driver driver = aRoute.getDriver();
-		// update rating and past trips
-		double currentRating=driver.getAvgRating();
-		double numPastTrips=(double)driver.getNumOfPastTrips();
-		currentRating= ((rating+currentRating*(numPastTrips))/(numPastTrips+1));
-		
-		driver.setNumOfPastTrips((int)numPastTrips+1);
-		driver.setAvgRating(currentRating);
-	
-}
-
 
 	@PostMapping("/participants/{name}")
 	public String createUser(@PathVariable("name") String userName) {
@@ -88,7 +31,7 @@ public void passengerRatesDriver (double rating, Route aRoute) {
 		return user.getUserName();
 	}
 
-	/*@GetMapping("/participants/{name}")
+	@GetMapping("/participants/{name}")
 	public String queryUser(@PathVariable("name") String userName) {
 		User user = repository.getUser(userName);
 		if(user == null) {
@@ -96,6 +39,6 @@ public void passengerRatesDriver (double rating, Route aRoute) {
 		}
 		return user.getUserName();
 	}
-	*/
+	
 
 }
--- src/main/java/ca/mcgill/ecse321/ridesharing/repository/RideSharingRepository.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/repository/RideSharingRepository.java
@@ -1,11 +1,18 @@
 package ca.mcgill.ecse321.ridesharing.repository;
 
+import java.util.Set;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import ca.mcgill.ecse321.ridesharing.model.Driver;
+import ca.mcgill.ecse321.ridesharing.model.Passenger;
+import ca.mcgill.ecse321.ridesharing.model.Request;
+import ca.mcgill.ecse321.ridesharing.model.Route;
+import ca.mcgill.ecse321.ridesharing.model.Status;
 import ca.mcgill.ecse321.ridesharing.model.User;
 
 @Repository
@@ -13,7 +20,6 @@ public class RideSharingRepository {
 
 	@Autowired
 	EntityManager entityManager;
-	
 
 	@Transactional
 	public User createUser(String userName) {
@@ -26,12 +32,76 @@ public class RideSharingRepository {
 		return participant;
 	}
 
-	/*@Transactional
+	@Transactional
 	public User getUser(String name) {
 		User participant = entityManager.find(User.class, name);
 		return participant;
-		
-	}
-	*/
 
-} 
+	}
+
+	// this method updates the seats available in a route depending on the status of
+	// the route
+	@Transactional
+	public void updateSeatsAvail(Route aRoute) {
+		int seatsAvail = aRoute.getSeatsAvailable();
+		Set<Request> requests = aRoute.getRequest();
+		for (Request r : requests) {
+			Status s = r.getStatus();
+			if (s == s.Accepted) {
+				seatsAvail--;
+			} else if (s == s.Ended) {
+				seatsAvail++;
+			}
+
+		}
+		aRoute.setSeatsAvailable(seatsAvail);
+	}
+
+	// this method should be called when a passenger is dropped off
+	// this gives the driver the chance to rate the passenger
+	// this also increments the num of past trips for the passenger and updates his
+	// rating
+	// this also updates the status of the specific request of the passenger to
+	// ended
+	@Transactional
+	public void passengerDroppedOff(Route aRoute, double rating, Passenger aPassenger) {
+
+		Set<Request> requests = aRoute.getRequest();
+		for (Request r : requests) {
+
+			Passenger p = r.getPassenger();
+			if (p == aPassenger) {
+				r.setStatus(Status.Ended);
+				double currentRating = aPassenger.getAvgRating();
+				double numPastTrips = (double) aPassenger.getNumOfPastTrips();
+
+				currentRating = ((rating + currentRating * (numPastTrips)) / (numPastTrips + 1));
+				aPassenger.setNumOfPastTrips((int) numPastTrips + 1);
+				aPassenger.setAvgRating(currentRating);
+			}
+
+		}
+	}
+
+	// A passenger will call this when he's dropped off and status of request is
+	// ended
+	// the route only has one driver so it is not necessary to take in a role
+	// this updates the drivers rating and the number of trips
+	// this should only be called when the passengerDroppedOff method has been
+	// called
+	// the status MUST BE = ENDED
+	@Transactional
+	public void passengerRatesDriver(double rating, Route aRoute) {
+
+		Driver driver = aRoute.getDriver();
+		// update rating and past trips
+		double currentRating = driver.getAvgRating();
+		double numPastTrips = (double) driver.getNumOfPastTrips();
+		currentRating = ((rating + currentRating * (numPastTrips)) / (numPastTrips + 1));
+
+		driver.setNumOfPastTrips((int) numPastTrips + 1);
+		driver.setAvgRating(currentRating);
+
+	}
+
+}
