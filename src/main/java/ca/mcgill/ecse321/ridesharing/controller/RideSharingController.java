a4b34b4ab012dc88f810453651b40c25c122b63f
Eden Ovadia <eden.ovadia@mail.mcgill.ca>
2018-10-07 15:17:40 -0400

    controller method for available seats

--- src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/controller/RideSharingController.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.ridesharing.controller;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,11 +9,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import ca.mcgill.ecse321.ridesharing.model.User;
+import ca.mcgill.ecse321.ridesharing.model.*;
 import ca.mcgill.ecse321.ridesharing.repository.RideSharingRepository;
 
 @RestController
 public class RideSharingController {
+
+	
 
 	@Autowired
 	RideSharingRepository repository;
@@ -20,6 +24,22 @@ public class RideSharingController {
 	public String greeting() {
 		return "Hello world!";
 	}
+	// this method updates the seats available in a route depending on the status of the route
+public void updateSeatsAvail (Route aRoute) {
+	int seatsAvail= aRoute.getSeatsAvailable();
+	Set <Request> requests = aRoute.getRequest();
+	for(Request r: requests) {
+		Status s = r.getStatus();
+		if (s==s.Accepted) {
+			seatsAvail--;
+		}
+		else if (s==s.Ended) {
+			seatsAvail++;
+		}
+		
+	}
+	aRoute.setSeatsAvailable(seatsAvail);
+}
 
 //	@PostMapping("/participants/{name}")
 //	public String createParticipant(@PathVariable("name") String userName) {
