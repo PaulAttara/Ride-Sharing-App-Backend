a508742450af81d04201606b931e1d500f093692
Noam Suissa <noam.suissa01@gmail.com>
2018-10-07 20:06:09 -0400

    Merge branch 'master' of https://github.com/ECSE321-Fall2018/t13

--- src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
@@ -4,10 +4,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import ca.mcgill.ecse321.ridesharing.controller.*;
+import ca.mcgill.ecse321.ridesharing.model.*;
+import ca.mcgill.ecse321.ridesharing.repository.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EventregistrationApplicationTests {
+public class RideSharingApplicationTests {
 
 	@Test
 	public void contextLoads() {
