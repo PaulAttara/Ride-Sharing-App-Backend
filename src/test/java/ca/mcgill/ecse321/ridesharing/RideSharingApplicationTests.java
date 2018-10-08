9a7ae832e55fce80dc348c98a237a459fd8e2d3a
ogKamy <kamy.moussavi@gmail.com>
2018-10-07 22:38:20 -0400

    scrap

--- src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
+++ src/test/java/ca/mcgill/ecse321/ridesharing/RideSharingApplicationTests.java
@@ -10,9 +10,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.invocation.InvocationOnMock;
-//import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.Extension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+//import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 import ca.mcgill.ecse321.ridesharing.controller.*;
 import ca.mcgill.ecse321.ridesharing.model.*;
@@ -22,9 +26,32 @@ import ca.mcgill.ecse321.ridesharing.repository.*;
 @SpringBootTest
 public class RideSharingApplicationTests {
 
+	
+
 	@Test
 	public void contextLoads() {
 
 	}
+	
+	@Mock 
+	private RideSharingRepository userDao;
+	
+	@InjectMocks
+	private RideSharingController controller;
 
+	private static final String USER_KEY = "TestUser";
+	private static final String NONEXISTING_Key = "NotAUser";
+	
+//	@BeforeEach
+//	void setMockOutput() {
+//	  when(userDao.getUser(anyString())).then( (InvocationOnMock invocation) -> {
+//	    if(invocation.getArgumentAt(0).equals(USER_KEY)) {
+//	      User user = new User();
+//	      participant.setName(USER_KEY);
+//	      return participant;
+//	    } else {
+//	      return null;
+//	    }
+//	  });
+//	}
 }
