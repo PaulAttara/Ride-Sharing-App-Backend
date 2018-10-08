6defd7cce61f4e949326b91bfd091403b9f0c1b3
ogKamy <kamy.moussavi@gmail.com>
2018-10-07 22:03:34 -0400

    fixed imports

--- src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
+++ src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
@@ -1,5 +1,7 @@
 package ca.mcgill.ecse321.ridesharing;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RideSharingApplication{
