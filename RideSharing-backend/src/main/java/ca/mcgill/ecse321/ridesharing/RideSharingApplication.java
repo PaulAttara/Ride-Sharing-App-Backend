17d5417d4669c1b96372a035b4bea18c98963dd3
Noam Suissa <noam.suissa01@gmail.com>
2018-10-24 14:07:48 -0400

    test commit

--- RideSharing-backend/src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
+++ RideSharing-backend/src/main/java/ca/mcgill/ecse321/ridesharing/RideSharingApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+//test
 //@EntityScan("ca.mcgill.ecse321.model")
 @SpringBootApplication//(scanBasePackages = {"ca.mcgill.ecse321.ridesharing.controller", "ca.mcgill.ecse321.ridesharing.model","ca.mcgill.ecse321.ridesharing.repository"})
 public class RideSharingApplication {
