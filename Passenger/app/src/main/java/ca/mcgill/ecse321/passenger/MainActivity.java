40e828e3a434d7fc4c6f8471f36b532b6fe0106b
Paul Attara <paul.attara@mail.mcgill.ca>
2018-11-09 01:26:25 -0500

    Modified Faulty UI Elements

--- Passenger/app/src/main/java/ca/mcgill/ecse321/passenger/MainActivity.java
+++ Passenger/app/src/main/java/ca/mcgill/ecse321/passenger/MainActivity.java
@@ -92,6 +92,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         return super.onOptionsItemSelected(item);
     }
+
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
